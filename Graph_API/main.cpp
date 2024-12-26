f9cf336682cef437340d543ddcc421cb98a72f21
ChanningTong <Channing_TongCN@outlook.com>
2024-12-26 19:58:07 +0800

    Initial Commit

---
+++ Graph_API/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Graph_API
+//
+//  Created by ChanningTong on 12/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
