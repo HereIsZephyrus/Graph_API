35652676e3e48a96021b1cf91304476b9979a85b
ChanningTong <Channing_TongCN@outlook.com>
2025-01-01 18:55:05 +0800

    add program scheme

--- Graph_API/main.cpp
+++ Graph_API/main.cpp
@@ -11,10 +11,15 @@
 #include "graph_api.hpp"
 
 int main(int argc, char **argv) {
-    //::testing::InitGoogleTest(&argc, argv);
+    if (argc > 1){
+        std::string program_type = argv[1];
+        if (program_type == "utest"){
+            ::testing::InitGoogleTest(&argc, argv);
+            return RUN_ALL_TESTS();
+        }
+    }
     WUSG::Graph<double> graph(87575);
     WUSG::CreateGraphFromFile("/Users/channingtong/Program/Graph_API/usa.txt", graph,true);
     std::cout<<graph.vertexCount()<<' '<<graph.edgeCount()<<std::endl;
-    //return RUN_ALL_TESTS();
     return 0;
 }
